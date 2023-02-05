742568c932260ff2619da0b257a0c2b82cd5daac
wannaflyhigh <joeydai901108@gmail.com>
2023-02-06 00:43:42 +0800

    詳解ok

--- Assets/Scripts/QuestionHandler.cs
+++ Assets/Scripts/QuestionHandler.cs
@@ -5,96 +5,132 @@ using UnityEngine.UI;
 
 public class QuestionHandler : MonoBehaviour
 {
-	public GameObject correctPanel;
-	public GameObject wrongPanel;
+    public GameObject correctPanel;
 
-	public GameObject close;
-	public GameObject Answered_Warn;
-	public Text ScoreText;
-	public GameObject CloseRightWrongBtn;
+    public GameObject wrongPanel;
 
-	public GameObject btnA;
-	public GameObject btnB;
-	public GameObject btnC;
-	public GameObject btnD;
+    public GameObject close;
 
-	public int Ans;
-	int nowQuestion;
-	int score=0;
+    public GameObject Answered_Warn;
 
-	public bool[] haveAnswered;
+    public Text ScoreText;
 
-	public void getStudentAns(int studentAns){
-		if(haveAnswered[nowQuestion-1]==true){
-			// now question have been answered
-			Answered_Warn.SetActive(true);
-			return;
-		}
-		CloseRightWrongBtn.SetActive(true);
-		if(studentAns==Ans){
-			correct();
-		}else{
-			wrong();
-		}
-		haveAnswered[nowQuestion-1]=true;
-	}
+    public GameObject CloseRightWrongBtn;
 
-	public void setAns(int ans){
-		Ans=ans;
-		correctPanel.SetActive(false);
-		wrongPanel.SetActive(false);
-	}
+    public GameObject btnA;
 
-	public void setNowQuestion(int questionID){
-		nowQuestion=questionID;
-	}
+    public GameObject btnB;
 
-	public void correct(){
-		score++;
-		ScoreText.text=""+score;
-		correctPanel.SetActive(true);
-		wrongPanel.SetActive(false);
-	}
+    public GameObject btnC;
 
-	public void wrong(){
-		wrongPanel.SetActive(true);
-		correctPanel.SetActive(false);
-	}
+    public GameObject btnD;
 
-	public void showBtns(){
-		btnA.SetActive(true);
-		btnB.SetActive(true);
-		btnC.SetActive(true);
-		btnD.SetActive(true);
-	}
+    public GameObject DetailAnsButton;
 
-	public void hideAll(){
-		btnA.SetActive(false);
-		btnB.SetActive(false);
-		btnC.SetActive(false);
-		btnD.SetActive(false);
-		correctPanel.SetActive(false);
-		wrongPanel.SetActive(false);
-		Answered_Warn.SetActive(false);
-		CloseRightWrongBtn.SetActive(false);
-	}
+    public GameObject DetailAnsPanel;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		hideAll();
-		haveAnswered=new bool[24];
-		for(int i=0;i<24;i++){
-			haveAnswered[i]=false;
-		}
-		ScoreText.text="0";
-	}
+    public Sprite[] detailAnsImages;
 
-	// Update is called once per frame
-	void Update()
-	{
-		
-	}
+    public int Ans;
 
-	
+    int nowQuestion;
+
+    int score = 0;
+
+    public bool[] haveAnswered;
+
+    public void getStudentAns(int studentAns)
+    {
+        if (haveAnswered[nowQuestion - 1] == true)
+        {
+            // now question have been answered
+            Answered_Warn.SetActive(true);
+            return;
+        }
+        CloseRightWrongBtn.SetActive(true);
+        if (studentAns == Ans)
+        {
+            correct();
+        }
+        else
+        {
+            wrong();
+        }
+        haveAnswered[nowQuestion - 1] = true;
+    }
+
+    public void setAns(int ans)
+    {
+        Ans = ans;
+        correctPanel.SetActive(false);
+        wrongPanel.SetActive(false);
+    }
+
+    public void setNowQuestion(int questionID)
+    {
+        nowQuestion = questionID;
+    }
+
+    public void correct()
+    {
+        score++;
+        ScoreText.text = "" + score;
+        correctPanel.SetActive(true);
+        wrongPanel.SetActive(false);
+        DetailAnsButton.SetActive(true);
+    }
+
+    public void wrong()
+    {
+        wrongPanel.SetActive(true);
+        correctPanel.SetActive(false);
+        DetailAnsButton.SetActive(true);
+    }
+
+    public void showBtns()
+    {
+        btnA.SetActive(true);
+        btnB.SetActive(true);
+        btnC.SetActive(true);
+        btnD.SetActive(true);
+    }
+
+    public void hideAll()
+    {
+        btnA.SetActive(false);
+        btnB.SetActive(false);
+        btnC.SetActive(false);
+        btnD.SetActive(false);
+        correctPanel.SetActive(false);
+        wrongPanel.SetActive(false);
+        Answered_Warn.SetActive(false);
+        CloseRightWrongBtn.SetActive(false);
+        DetailAnsButton.SetActive(false);
+        DetailAnsPanel.SetActive(false);
+    }
+
+    public void showDetailAnsPanel()
+    {
+        hideAll();
+        DetailAnsPanel.SetActive(true);
+        Image image = DetailAnsPanel.GetComponent<Image>();
+        image.sprite = detailAnsImages[nowQuestion - 1];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideAll();
+        haveAnswered = new bool[24];
+        for (int i = 0; i < 24; i++)
+        {
+            haveAnswered[i] = false;
+        }
+        ScoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 }
