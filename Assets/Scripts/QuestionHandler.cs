d23b592ecdb054548dd920a87144a845a171386f
wannaflyhigh <joeydai901108@gmail.com>
2023-02-13 17:36:40 +0800

    詳解

--- Assets/Scripts/QuestionHandler.cs
+++ Assets/Scripts/QuestionHandler.cs
@@ -39,12 +39,36 @@ public class QuestionHandler : MonoBehaviour
 
     public bool[] haveAnswered;
 
+    public bool[] haveAnsweredAndRight;
+
+    // public GameObject[] badges;
+    public GameObject badgeTW;
+
+    public GameObject badgeChild;
+
+    public GameObject badgeRainForest;
+
+    public GameObject badgeAustralia;
+
+    public GameObject badgeDesert;
+
+    public GameObject badgeAfica;
+
+    public GameObject badgeBird;
+
+    public GameObject badgeClimbBug;
+
+    public GameObject badgeWarm;
+
+    public int[] Answers;
+
     public void getStudentAns(int studentAns)
     {
         if (haveAnswered[nowQuestion - 1] == true)
         {
             // now question have been answered
             Answered_Warn.SetActive(true);
+			DetailAnsButton.SetActive(true);
             return;
         }
         CloseRightWrongBtn.SetActive(true);
@@ -59,25 +83,62 @@ public class QuestionHandler : MonoBehaviour
         haveAnswered[nowQuestion - 1] = true;
     }
 
-    public void setAns(int ans)
+    public void initQuestion(int questionID)
     {
-        Ans = ans;
+        hideAll();
+        nowQuestion = questionID;
+        Ans = Answers[questionID - 1];
         correctPanel.SetActive(false);
         wrongPanel.SetActive(false);
-    }
-
-    public void setNowQuestion(int questionID)
-    {
-        nowQuestion = questionID;
+        showBtns();
     }
 
     public void correct()
     {
         score++;
         ScoreText.text = "" + score;
+		haveAnsweredAndRight[nowQuestion-1]=true;
         correctPanel.SetActive(true);
         wrongPanel.SetActive(false);
         DetailAnsButton.SetActive(true);
+        if (
+            haveAnsweredAndRight[0] &&
+            haveAnsweredAndRight[1] &&
+            haveAnsweredAndRight[2]
+        ) badgeTW.SetActive(true);
+        if (
+            haveAnsweredAndRight[3] &&
+            haveAnsweredAndRight[4] &&
+            haveAnsweredAndRight[5]
+        ) badgeChild.SetActive(true);
+        if (
+            haveAnsweredAndRight[6] &&
+            haveAnsweredAndRight[7] &&
+            haveAnsweredAndRight[8]
+        ) badgeRainForest.SetActive(true);
+        if (haveAnsweredAndRight[9] && haveAnsweredAndRight[11])
+            badgeAustralia.SetActive(true);
+        if (haveAnsweredAndRight[10]) badgeDesert.SetActive(true);
+        if (
+            haveAnsweredAndRight[12] &&
+            haveAnsweredAndRight[13] &&
+            haveAnsweredAndRight[14]
+        ) badgeAfica.SetActive(true);
+        if (
+            haveAnsweredAndRight[15] &&
+            haveAnsweredAndRight[16] &&
+            haveAnsweredAndRight[17]
+        ) badgeBird.SetActive(true);
+        if (
+            haveAnsweredAndRight[18] &&
+            haveAnsweredAndRight[19] &&
+            haveAnsweredAndRight[20]
+        ) badgeClimbBug.SetActive(true);
+        if (
+            haveAnsweredAndRight[21] &&
+            haveAnsweredAndRight[22] &&
+            haveAnsweredAndRight[23]
+        ) badgeWarm.SetActive(true);
     }
 
     public void wrong()
@@ -122,9 +183,11 @@ public class QuestionHandler : MonoBehaviour
     {
         hideAll();
         haveAnswered = new bool[24];
+        haveAnsweredAndRight = new bool[24];
         for (int i = 0; i < 24; i++)
         {
             haveAnswered[i] = false;
+            haveAnsweredAndRight[i] = false;
         }
         ScoreText.text = "0";
     }
